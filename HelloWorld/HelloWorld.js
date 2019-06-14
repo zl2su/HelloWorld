09446a8fdc2d1d791aab9d1564576f43dc8b50e6
zl2su <51268407+zl2su@users.noreply.github.com>
2019-06-14 22:33:13 +0900

    Add files via upload

---
+++ HelloWorld/HelloWorld.js
@@ -0,0 +1,13 @@
+﻿module.exports=function(RED){
+	function helloworld(config){
+		RED.nodes.createNode(this, config);
+		
+		var context = this.context();
+		var node = this;
+		
+		this.on('input', function(msg){
+			var outMsg = {payload: "HelloWorld"};
+			node.send(outMsg);
+		});
+	}RED.nodes.registerType("HelloWorld", helloworld);
+}; 
